ac765d9e57d0dbfeb715528040a5dfddca195820
itohdak <itohdak@gmail.com>
2018-05-21 01:57:00 +0900

    solve AGC024 B & add link of test cases to README

--- AtCoder_Grand_Contest/024/B.cpp
+++ AtCoder_Grand_Contest/024/B.cpp
@@ -11,12 +11,31 @@ using namespace std;
 int main(){
   int N;
   cin >> N;
-  vector<int> P;
+  int Q[N];
   for(int i=0; i<N; i++){
-    int in;
-    cin >> in;
-    P.push_back(in);
+    int p;
+    cin >> p;
+    Q[p-1] = i;
   }
-  
+  int max = 1;
+  int tmp = Q[0];
+  int n = 1;
+  for(int i=1; i<N; i++){
+    if(tmp < Q[i]){
+      n++;
+      tmp = Q[i];
+    } else {
+      if(max < n)
+	max = n;
+      tmp = Q[i];
+      n = 1;
+    }
+
+    if(i == N-1)
+      if(max < n)
+	max = n;
+  }
+
+  cout << N - max << endl;
   return 0;
 }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [AtCoder Home](https://atcoder.jp/?lang=ja "AtCoder Home")
 - [AtCoder Problems](https://kenkoooo.com/atcoder/?user=itohdak&rivals=&kind=category "AtCoder Problems")
 - [itohdak Profile](https://atcoder.jp/user/itohdak "itohdak Profile")
+- [test cases](https://www.dropbox.com/sh/arnpe0ef5wds8cv/AAAk_SECQ2Nc6SVGii3rHX6Fa?dl=0 "test cases")
