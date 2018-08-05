8b8befa09846408e74e9ca4e03b8584062cb1ec1
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2018-08-05 19:36:03 +0900

    moved ABC062 & still solving ABC095 D

--- AtCoder_Beginner_Contest/unsolved/09-/095/D.cpp
+++ AtCoder_Beginner_Contest/unsolved/09-/095/D.cpp
@@ -9,54 +9,63 @@ using namespace std;
 #define ll long long
 
 int main(){
-  ll N, C;
+  int N;
+  ll C;
   cin >> N >> C;
-  vector<ll> X, V;
-  bool flag[N];
-  for(ll i=0; i<N; i++){
-    ll x, v;
-    cin >> x >> v;
-    X.push_back(x);
-    V.push_back(v);
-    flag[i] = true;
-  }
-  ll pos = 0;
-  ll ret = 0;
-  while(1){
-    ll maxv = -10000;
-    ll maxi = -1;
-    for(int i=0; i<N; i++){
-      if(flag[i]){
-	if(V[i] - min(abs(X[i]-pos), C-abs(X[i]-pos)) > maxv){
-	  maxv = V[i] - min(abs(X[i]-pos), C-abs(X[i]-pos));
-	  maxi = i;
-	}
-      }
-    }
-    if(maxv < 0){
-      break;
+  vector<ll> X(N+1, 0), V(N);
+  for(int i=0; i<N; i++)
+    cin >> X[i+1] >> V[i];
+  vector<ll> sum(N+1, 0);
+  sum[0] = 0;
+  for(int i=1; i<=N; i++)
+    sum[i] = sum[i-1] + V[i-1];
+  vector<ll> F(N+1, 0);
+  for(int i=0; i<=N; i++)
+    F[i] = sum[i] - X[i];
+  vector<ll> G(N+1, 0);
+  vector<ll> Gpos(N+1, 0);
+  G[0] = 0;
+  Gpos[0] = 0;
+  G[1] = 0;
+  Gpos[1] = 0;
+  for(int j=2; j<=N; j++)
+    if(G[j-1] < F[j-1]){
+      G[j] = F[j-1];
+      Gpos[j] = j-1;
     } else {
-      for(int i=0; i<N; i++){
-	if(flag[i]){
-	  if((X[maxi] > pos && X[maxi]-pos == min(abs(X[maxi]-pos), C-abs(X[maxi]-pos)) &&
-	      X[i] > pos && X[i] < X[maxi]) ||
-	     (X[maxi] < pos && pos-X[maxi] == min(abs(X[maxi]-pos), C-abs(X[maxi]-pos)) &&
-	      X[i] < pos && X[i] > X[maxi]) ||
-	     (X[maxi] < pos && pos-X[maxi] == max(abs(X[maxi]-pos), C-abs(X[maxi]-pos)) &&
-	      (X[i] > pos || X[i] < X[maxi])) ||
-	     (X[maxi] > pos && X[maxi]-pos == max(abs(X[maxi]-pos), C-abs(X[maxi]-pos)) &&
-	      (X[i] < pos || X[i] > X[maxi]))
-	     ){
-	    ret += V[i];
-	    flag[i] = false;
-	  }
-	}
-      }
-      ret += maxv;
-      flag[maxi] = false;
-      pos = X[maxi];
+      G[j] = G[j-1];
+      Gpos[j] = Gpos[j-1];
+    }
+  ll ans = 0;
+  for(int j=1; j<=N; j++){
+    ll tmp = sum[N] - sum[j-1] - (C - X[j]) + G[j] - min(C - X[j], X[Gpos[j]]);
+    if(tmp > ans){
+      ans = tmp;
+    }
+    ll tmp2 = sum[j] - X[j];
+    if(tmp2 > ans){
+      ans = tmp2;
     }
   }
-  cout << ret << endl;
+  // for(int i=0; i<=N; i++){
+  //   for(int j=i; j<=N; j++){
+  //     ll tmp = 0;
+  //     if(i == 0){
+  // 	if(j == 0){
+  // 	  tmp = 0;
+  // 	} else {
+  // 	  tmp = max(sum[j] - X[j], (sum[N] - sum[j-1]) - (C - X[j]));
+  // 	}
+  //     } else if(i != j){
+  // 	tmp = (sum[N] - sum[j-1] + sum[i]) - (C - X[j] + X[i]) - min(C - X[j], X[i]);
+  //     }
+  //     if(tmp > ans){
+  // 	ans = tmp;
+  // 	// cout << i << ' ' << j << endl;
+  //     }
+  //   }
+  // }
+  cout << ans << endl;
   return 0;
 }
+
