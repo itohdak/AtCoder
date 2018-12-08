ba94b89e3723c881da5b25e468e934b47494e457
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2018-12-09 04:53:03 +0900

    add find_divisor function in Prime.cpp

--- AtCoder_Beginner_Contest/Prime/Prime.cpp
+++ AtCoder_Beginner_Contest/Prime/Prime.cpp
@@ -101,3 +101,21 @@ void factorization(ll N) {
 
   return;
 }
+
+
+
+/*
+  約数
+*/
+vector<ll> divisor;
+void find_divisor(ll N) {
+  for(int i=1; i<ceil(sqrt(N))+1; i++) {
+    if(N % i == 0)
+      divisor.push_back(i);
+  }
+  int n = divisor.size();
+  for(int i=n-1; i>=0; i--) {
+    if(N != (ll)pow(divisor[i], 2))
+      divisor.push_back(N / divisor[i]);
+  }
+}
