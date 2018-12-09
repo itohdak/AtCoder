49980ba35fb0fe1914a8ad07c0fb2b88a6f70aeb
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2018-12-09 16:25:19 +0900

    solve ABC 108-AB

--- AtCoder_Beginner_Contest/108/A.cpp
+++ AtCoder_Beginner_Contest/108/A.cpp
@@ -9,6 +9,9 @@ using namespace std;
 #define ll long long
 
 int main(){
+  int K;
+  cin >> K;
+  cout << K/2 * (K-K/2) << endl;
   return 0;
 }
 
--- AtCoder_Beginner_Contest/108/B.cpp
+++ AtCoder_Beginner_Contest/108/B.cpp
@@ -9,6 +9,11 @@ using namespace std;
 #define ll long long
 
 int main(){
+  int x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  int dx = x2 - x1;
+  int dy = y2 - y1;
+  cout << x2 - dy << ' ' << y2 + dx << ' ' << x1 - dy << ' ' << y1 + dx << endl;
   return 0;
 }
 
