700db036b41d9d9a5a31536f35bcc64c94b2f6d6
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2018-12-09 17:23:13 +0900

    solve ABC 105-D

--- AtCoder_Beginner_Contest/previous/10-/105/D.cpp
+++ AtCoder_Beginner_Contest/previous/10-/105/D.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <vector>
 #include <numeric> // accumulate(v.begin(), v.end(), 0)
+#include <map>
 
 using namespace std;
 #define ll long long
@@ -20,22 +21,21 @@ int main(){
   for(int i=0; i<N; i++)
     B[i+1] = B[i] + A[i];
 
-  ll ans = 0LL;
-  vector< vector<ll> > memo(N, vector<ll>(0));
-  for(int i=0; i<N; i++){
-    ll tmp = A[i];
-    if(i != 0){
-      for(int j=0; j<memo[i-1].size(); j++){
-	if(memo[i-1][j]%M == 0)
-	  ans++;
-	memo[i].push_back((memo[i-1][j]+tmp)%M);
-      }
-    }
-    memo[i].push_back(tmp%M);
+  map<ll, int> m1;
+  vector<ll> m2;
+  for(int i=0; i<N+1; i++) {
+    ll mod = B[i] % M;
+    m1[mod];
+    if(m1[mod] == 0)
+      m2.push_back(mod);
+    m1[mod]++;
   }
-  for(int j=0; j<memo[N-1].size(); j++)
-    if((memo[N-1][j])%M == 0)
-      ans++;
+
+  ll ans = 0;
+  for(int i=0; i<m2.size(); i++) {
+    ll num = m1[m2[i]];
+    ans += num * (num - 1) / 2;
+  }
 
   cout << ans << endl;
   return 0;
