58d61bd567c10a4982ce7704dc715993f54bb747
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2018-12-09 18:50:50 +0900

    solve ABC 104-CD

--- AtCoder_Beginner_Contest/104/C.cpp
+++ AtCoder_Beginner_Contest/104/C.cpp
@@ -8,15 +8,34 @@
 using namespace std;
 #define ll long long
 
+int D;
+ll G;
+int P[10];
+ll C[10];
+int ans = 1000;
+
+void select_problem(int t, int i=0, int n=0, ll tmp=0LL) {
+  // cout << t << ' ' << i << ' ' << n << ' ' << tmp << endl;
+  if(i == D) {
+    // cout << ceil(max((G - tmp), 0LL) / 100.0*(t+1)) << endl;
+    if(tmp + 100*(t+1)*P[t] >= G && n + ceil(max((G - tmp), 0LL) / (100.0*(t+1))) < ans) {
+      ans = n + ceil(max((G - tmp), 0LL) / (100.0*(t+1)));
+    }
+    return;
+  } else {
+    if(i != t)
+      select_problem(t, i+1, n+P[i], tmp+100*(i+1)*P[i]+C[i]);
+    select_problem(t, i+1, n, tmp);
+  }
+}
+
 int main(){
-  int D;
-  ll G;
   cin >> D >> G;
-  vector<int> P(D);
-  vector<ll> C(D);
   for(int i=0; i<D; i++)
     cin >> P[i] >> C[i];
-  
+  for(int i=0; i<D; i++)
+    select_problem(i);
+  cout << ans << endl;
   return 0;
 }
 
--- AtCoder_Beginner_Contest/104/D.cpp
+++ AtCoder_Beginner_Contest/104/D.cpp
@@ -7,47 +7,51 @@
 
 using namespace std;
 #define ll long long
+#define MOD 1000000007
+
+ll dp[100000][4];
 
 int main(){
   string S;
   cin >> S;
   int N = (int)S.size();
-  vector<int> A(N+1, 0), B(N+1, 0), C(N+1, 0), D;
-  A[N] = B[N] = C[N] = 0;
-  for(int i=N-1; i>=0; i--){
-    A[i] = A[i+1];
-    B[i] = B[i+1];
-    C[i] = C[i+1];
-    if(S[i] == 'A')
-      A[i]++;
-    else if(S[i] == 'B')
-      B[i]++;
-    else if(S[i] == 'C')
-      C[i]++;
-    else {
-      A[i]++; B[i]++; C[i]++;
-      D.push_back(i);
+  dp[0][0] = 1;
+  for(int i=1; i<=N; i++) {
+    if(S[i-1] == '?') {
+      dp[i][0] = 3 * dp[i-1][0];
+      dp[i][1] = 3 * dp[i-1][1];
+      dp[i][2] = 3 * dp[i-1][2];
+      dp[i][3] = 3 * dp[i-1][3];
+    } else {
+      dp[i][0] = dp[i-1][0];
+      dp[i][1] = dp[i-1][1];
+      dp[i][2] = dp[i-1][2];
+      dp[i][3] = dp[i-1][3];
     }
-  }
-  D.push_back(D[D.size()-1]-1);
-  reverse(D.begin(), D.end());
-  D.push_back(D[D.size()-1]+1);
-  // for(int i=0; i<D.size(); i++)
-  //   cout << D[i] << ' ';
-  // cout << endl;
-  ll sum = 0;
-  for(int i=0; i<D.size(); i++){
-    for(int j=i+1; j<D.size(); j++){
-      // cout << D[i] << ' ' << D[j] << " : ";
-      // cout << (A[0] - A[D[i]]) << ' ' << (B[D[i]] - B[D[j]]) << ' ' << C[D[j]] << endl;
-      for(int a=0; a<i; a++)
-	for(int b=0; b<j-i; b++)
-	  for(int c=0; c<D.size()-j; c++)
-	    sum += (A[0] - A[D[i]] - a) * (B[D[i]] - B[D[j]] - b) * (C[D[j]] - c);
-      sum %= 1000000007;
+    if(S[i-1] == 'A')
+      dp[i][1] += dp[i-1][0];
+    else if(S[i-1] == 'B')
+      dp[i][2] += dp[i-1][1];
+    else if(S[i-1] == 'C')
+      dp[i][3] += dp[i-1][2];
+    else { // S[i-1] == '?'
+      dp[i][1] += dp[i-1][0];
+      dp[i][2] += dp[i-1][1];
+      dp[i][3] += dp[i-1][2];
     }
+
+    dp[i][0] %= MOD;
+    dp[i][1] %= MOD;
+    dp[i][2] %= MOD;
+    dp[i][3] %= MOD;
   }
-  cout << sum << endl;
+  // for(int i=0; i<N+1; i++) {
+  //   for(int j=0; j<4; j++) {
+  //     cout << dp[i][j] << ' ';
+  //   }
+  //   cout << endl;
+  // }
+  cout << dp[N][3] << endl;
   return 0;
 }
 
