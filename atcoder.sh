e6af6c19a33f4fc30c8cbbdc24583803edcf6adc
Naoya Yamaguchi <708yamaguchi@gmail.com>
2019-06-09 22:11:56 +0900

    change path of mkdir

--- atcoder.sh
+++ atcoder.sh
@@ -2,10 +2,12 @@
 
 function mkdir_atcoder () {
     mkdir $PWD/$1
-    cp ~/AtCoder/000/A.cpp $PWD/$1/A.cpp
-    cp ~/AtCoder/000/A.cpp $PWD/$1/B.cpp
-    cp ~/AtCoder/000/A.cpp $PWD/$1/C.cpp
-    cp ~/AtCoder/000/A.cpp $PWD/$1/D.cpp
-    cp ~/AtCoder/000/A.cpp $PWD/$1/E.cpp
-    cp ~/AtCoder/000/A.cpp $PWD/$1/F.cpp
+    cp $(dirname $0)/000/A.cpp $PWD/$1/A.cpp
+    cp $(dirname $0)/000/A.cpp $PWD/$1/B.cpp
+    cp $(dirname $0)/000/A.cpp $PWD/$1/C.cpp
+    cp $(dirname $0)/000/A.cpp $PWD/$1/D.cpp
+    cp $(dirname $0)/000/A.cpp $PWD/$1/E.cpp
+    cp $(dirname $0)/000/A.cpp $PWD/$1/F.cpp
 }
+
+mkdir_atcoder $1
