2c96e7cdbf0aca5b40f5728f7d5853acd8c5b67f
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-10-26 03:48:34 +0900

    fix repl

--- 000/A.cpp
+++ 000/A.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define REP(i,m,n) for(int i=(int)(m); i<(int)(n); i++)
 #define rep(i,n) REP(i,0,n)
 #define REPL(i,m,n) for(ll i=(ll)(m); i<(ll)(n); i++)
-#define repl(i,n) REP(i,0,n)
+#define repl(i,n) REPL(i,0,n)
 #define all(v) v.begin(), v.end()
 const int inf = 1e9+7;
 const ll longinf = 1LL<<60;
