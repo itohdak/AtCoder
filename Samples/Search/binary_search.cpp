f95c30341eeee99196a7282955757889ed1de833
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-10-29 02:02:44 +0900

    modify binary search sample

--- Samples/Search/binary_search.cpp
+++ Samples/Search/binary_search.cpp
@@ -1,22 +1,28 @@
-#include <iostream>
-#include <cstdlib>
-#include <algorithm>
-#include <cmath>
-#include <vector>
-#include <numeric> // accumulate(v.begin(), v.end(), 0)
+#include <bits/stdc++.h>
 
 using namespace std;
 #define ll long long
 
-bool test(vector<int> v, int k, int t) {
+vector<ll> v;
+bool test(ll k, ll t) {
   return v[k] <= t; // modify here
 }
 
-int binary_search(vector<int> v, int n, int t) {
-  int l = -1, r = n; // 初期値注意
+ll binary_search(ll n, ll t) {
+  ll ng = n, ok = 0; // 初期値注意; ngは条件を満たす可能性のある数より1大きく(or小さく)
+  while(abs(ok - ng) > 1) {
+    ll mid = (ok + ng) / 2;
+    if(test(mid, t)) ok = mid;
+    else ng = mid;
+  }
+  return ok;
+}
+
+ll binary_search_old(ll n, ll t) {
+  ll l = -1, r = n; // 初期値注意
   while(r - l > 1) {
-    int mid = (l + r) / 2;
-    if(test(v, mid, t))
+    ll mid = (l + r) / 2;
+    if(test(mid, t))
       l = mid;
     else
       r = mid;
@@ -25,14 +31,15 @@ int binary_search(vector<int> v, int n, int t) {
 }
 
 int main() {
-  int a[] = {1,4,7,19,29};
-  vector<int> v(a, a+5);
+  ll a[] = {1,4,7,19,29};
+  v = vector<ll>(a, a+5);
   int n = v.size();
   cout << "input: ";
   for(int i=0; i<n; i++)
     cout << v[i] << ' ';
   cout << endl;
-  cout << binary_search(v, n, 1) << endl;
+  cout << binary_search(n, 1) << endl;
+  cout << binary_search_old(n, 1) << endl;
   cout << upper_bound(v.begin(), v.end(), 1) - v.begin() << endl; // upper_boundは条件を満たさない
   cout << lower_bound(v.begin(), v.end(), 1) - v.begin() << endl; // lower_boundは条件を満たす
   return 0;
