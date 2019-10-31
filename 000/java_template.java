6c659b7bf41923f39d85bbfed2182e7cb33b5f8a
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-11-01 04:14:59 +0900

    add java template

---
+++ 000/java_template.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // input
+        final int a = sc.nextInt();
+        final String s = sc.next();
+
+        // output
+        System.out.println("Hello World!")
+    }
+}
+
