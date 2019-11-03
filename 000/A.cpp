233681a6abc0c4eb8bed57d3aed566891afbe3dc
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-11-04 04:36:49 +0900

    add operator function to print vector and pair

--- 000/A.cpp
+++ 000/A.cpp
@@ -8,6 +8,15 @@ using namespace std;
 #define REPL(i,m,n) for(ll i=(ll)(m); i<(ll)(n); i++)
 #define repl(i,n) REPL(i,0,n)
 #define all(v) v.begin(), v.end()
+template<class t,class u>
+ostream& operator<<(ostream& os, const pair<t, u>& p) {
+  return os << "{" << p.first << ", " << p.second << "}";
+}
+template<class t> ostream& operator<<(ostream& os, const vector<t>& v) {
+  os << "{";
+  rep(i, v.size()) { if(i) os << ", "; os << v[i]; }
+  return os << "}";
+}
 const int inf = 1e9+7;
 const ll longinf = 1LL<<60;
 const ll mod = 1e9+7;
