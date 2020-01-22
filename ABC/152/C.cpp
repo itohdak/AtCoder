5350d52124f91adf15aa4129a51ce35de1375113
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2020-01-22 13:24:37 +0900

    add ABC152

---
+++ ABC/152/C.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define REP(i,m,n) for(int i=(int)(m); i<(int)(n); i++)
+#define rep(i,n) REP(i,0,n)
+#define RREP(i,m,n) for(int i=(int)(m); i>=(int)(n); i--)
+#define rrep(i,n) RREP(i,n-1,0)
+#define REPL(i,m,n) for(ll i=(ll)(m); i<(ll)(n); i++)
+#define repl(i,n) REPL(i,0,n)
+#define all(v) v.begin(), v.end()
+// template<class t, class u>
+// ostream& operator<<(ostream& os, const pair<t, u>& p) {
+//   return os << "{" << p.first << ", " << p.second << "}";
+// }
+// template<class t> ostream& operator<<(ostream& os, const vector<t>& v) {
+//   os << "{";
+//   rep(i, v.size()) { if(i) os << ", "; os << v[i]; }
+//   return os << "}";
+// }
+// template<class t> ostream& operator<<(ostream& os, const set<t>& se) {
+//   os << "{";
+//   auto itr = se.begin();
+//   rep(i, se.size()) { if(i) os << ", "; os << *itr; itr++; }
+//   return os << "}";
+// }
+// template<class t, class u>
+// ostream& operator<<(ostream& os, const map<t, u>& mp) {
+//   os << "{";
+//   auto itr = mp.begin();
+//   rep(i, mp.size()) { if(i) os << ", "; os << *itr; itr++; }
+//   return os << "}";
+// }
+const int inf = 1e9+7;
+const ll longinf = 1LL<<60;
+const ll mod = 1e9+7;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> P(N);
+  rep(i, N) cin >> P[i];
+  int cnt = 0;
+  int mn = N+1;
+  rep(i, N) {
+    if(P[i] < mn) {
+      cnt++;
+      mn = P[i];
+    }
+  }
+  cout << cnt << endl;
+  return 0;
+}
+
---
+++ ABC/152/E.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define REP(i,m,n) for(int i=(int)(m); i<(int)(n); i++)
+#define rep(i,n) REP(i,0,n)
+#define RREP(i,m,n) for(int i=(int)(m); i>=(int)(n); i--)
+#define rrep(i,n) RREP(i,n-1,0)
+#define REPL(i,m,n) for(ll i=(ll)(m); i<(ll)(n); i++)
+#define repl(i,n) REPL(i,0,n)
+#define all(v) v.begin(), v.end()
+// template<class t, class u>
+// ostream& operator<<(ostream& os, const pair<t, u>& p) {
+//   return os << "{" << p.first << ", " << p.second << "}";
+// }
+template<class t> ostream& operator<<(ostream& os, const vector<t>& v) {
+  os << "{";
+  rep(i, v.size()) { if(i) os << ", "; os << v[i]; }
+  return os << "}";
+}
+// template<class t> ostream& operator<<(ostream& os, const set<t>& se) {
+//   os << "{";
+//   auto itr = se.begin();
+//   rep(i, se.size()) { if(i) os << ", "; os << *itr; itr++; }
+//   return os << "}";
+// }
+// template<class t, class u>
+// ostream& operator<<(ostream& os, const map<t, u>& mp) {
+//   os << "{";
+//   auto itr = mp.begin();
+//   rep(i, mp.size()) { if(i) os << ", "; os << *itr; itr++; }
+//   return os << "}";
+// }
+const int inf = 1e9+7;
+const ll longinf = 1LL<<60;
+const ll mod = 1e9+7;
+
+ll gcd(ll x, ll y) {
+  if(x > y)
+    return gcd(y, x);
+  else if(x == 0)
+    return y;
+  else
+    return gcd(y % x, x);
+}
+ll lcm(ll x, ll y) {
+  ll g = gcd(x, y);
+  return g * (x / g) * (y / g);
+}
+
+int main() {
+  int N;
+  cin >> N;
+  vector<ll> A(N);
+  rep(i, N) cin >> A[i];
+  sort(all(A));
+  ll GCD = A[0];
+  rep(i, N) GCD = gcd(A[i], GCD);
+  vector<ll> B(N);
+  rep(i, N) B[i] = A[i] / GCD;
+  vector<ll> sum(N+1);
+  rep(i, N) sum[i+1] = sum[i] + B[i];
+  cout << B << endl;
+  cout << sum << endl;
+  ll ans = 1;
+  rrep(i, N) {
+    ans *= B[i] * sum[i];
+    ans %= mod;
+  }
+  cout << ans << endl;
+  return 0;
+}
