8fab7a15ba62705fb5e84d1b038365d48e748034
itohdak <itohdak@gmail.com>
2020-04-27 01:29:51 +0900

    add dijkstra in Samples

---
+++ Samples/Shortest_Path/dijkstra.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int inf = 1e9+7;
+const ll longinf = 1LL<<60;
+const ll mod = 1e9+7;
+
+struct edge {
+  int to;
+  ll cost;
+  edge(int t, ll c): to(t), cost(c) {};
+};
+
+using P = pair<int, ll>;
+
+int N;
+vector<vector<edge>> G;
+vector<ll> d;
+vector<int> par;
+void dijkstra(int s) {
+  priority_queue<P, vector<P>, greater<P> > que;
+  d = vector<ll>(N, longinf);
+  par = vector<int>(N, -1);
+  d[s] = 0;
+  que.push(P(0, s));
+
+  while (!que.empty()) {
+    P p = que.top();
+    que.pop();
+    int v = p.second;
+    if (d[v] < p.first) continue;
+
+    for (int i=0; i<(int)G[v].size(); ++i) {
+      edge e = G[v][i];
+      if (d[e.to] > d[v] + e.cost) {
+	d[e.to] = d[v] + e.cost;
+	que.push(P(d[e.to], e.to));
+	par[e.to] = v;
+      }
+    }
+  }
+}
