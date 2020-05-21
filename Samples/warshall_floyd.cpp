f3b6aa1a2f65389f788a2193a0efe0d7be0d34f9
itohdak <itohdak@gmail.com>
2020-05-22 00:21:47 +0900

    update samples

--- Samples/warshall_floyd.cpp
+++ Samples/warshall_floyd.cpp
@@ -7,12 +7,17 @@ const int inf = 1e9+7;
 const ll longinf = 1LL<<60;
 const ll mod = 1e9+7;
 
-typedef vector<vector<int> > Matrix;
+// グラフの距離を格納した2次元配列（隣接行列）
+// dist[u][v]は辺e=(u,v)のコスト（辺が存在しない場合はinf、ただしdist[i][i]=0）
+vector<vector<ll>> dist;
 
-Matrix dist; // グラフの距離を格納した2次元配列（隣接行列）
-             // dist[u][v]は辺e=(u,v)のコスト（辺が存在しない場合はinf、ただしdist[i][i]=0）
+void warshall_floyd(int n) {
+  rep(i, n) rep(j, n) rep(k, n) {
+    dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
+  }
+}
 
-void warshall_floyd(int n) { // nは頂点数
+void warshall_floyd_discription(int n) { // nは頂点数
   rep(i, n)      // 経由する頂点
     rep(j, n)    // 開始頂点
       rep(k, n)  // 終端
@@ -23,7 +28,7 @@ int main() {
   int n, m;
   cin >> n;
 
-  dist = Matrix(n, vector<int>(n, inf));
+  dist = vector<vector<ll>>(n, vector<ll>(n, longinf));
   rep(i, n) dist[i][i] = 0;
 
   cin >> m;
@@ -37,7 +42,7 @@ int main() {
 
   rep(i, n) {
     rep(j, n) {
-      if (i != j && dist[i][j] != inf)
+      if (i != j && dist[i][j] != longinf)
         cout << i << "から" << j << "へのコスト: " << dist[i][j] << endl;
     }
   }
